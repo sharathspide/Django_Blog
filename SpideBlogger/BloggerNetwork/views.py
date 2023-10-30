9d54f42d1ef5ab35b13365648ce12a314655c25c
RaSharath <rasharath619@gmail.com>
2023-10-30 20:51:10 +0530

    CMT-2 - UI and some basic Django work models

--- SpideBlogger/BloggerNetwork/views.py
+++ SpideBlogger/BloggerNetwork/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
+#All of our blog post - ListView
+#retrieve the single blog post -DetailView
 
-def home (request):
-    return render(request, 'Home.html',{})
+# def home (request):
+#     return render(request, 'Home.html',{})
+
+class HomeView (ListView):
+    model = Post
+    template_name = 'Home.html'
+
+class PostDetail (DetailView):
+    model = Post
+    template_name = 'Post_Detail.html'
